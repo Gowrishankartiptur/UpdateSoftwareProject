00795dea900e029a732cc31649a1e0ab0b68c450
Gowrishankartiptur <gowrishankartiptur@gmail.com>
2014-12-26 16:10:39 +0530

    Committed on 26/12/2014

---
+++ UpdateSoftware.py
@@ -0,0 +1,17 @@
+__author__ = 'Dr.S.Gowrishankar'
+#!/usr/bin/env python
+
+import os
+import pip
+
+dists = []
+for dist in pip.get_installed_distributions():
+    dists.append(dist.project_name)
+
+for dist_name in sorted(dists, key=lambda s: s.lower()):
+    #on linux
+    #cmd = "sudo pip install {0} -U".format(dist_name)
+    #on windows
+    cmd = "pip install {0} -U".format(dist_name)
+    print '#', cmd
+    os.system(cmd)
